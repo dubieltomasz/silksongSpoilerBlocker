ced881b666f81525167e6dd4cb92301abdeba158
Tomasz Dubiel <dubieltomasz420@gmail.com>
2025-09-06 13:50:10 +0200

    Resolved some performance issues and bugs

--- blockSpoilers.js
+++ blockSpoilers.js
@@ -1,3 +1,5 @@
+const content = document.getElementById('contents') || document.body;
+const youtubeVideoClasses = "ytd-rich-item-renderer, .yt-lockup-view-model, ytd-video-renderer, ytm-shorts-lockup-view-model-v2, .ytp-videowall-still, .shortsLockupViewModelHost, .ytGridShelfViewModelGridShelfItem";
 let blacklist = [];
 
 blacklist = JSON.parse(localStorage.getItem('blacklist'));
@@ -5,42 +7,32 @@ if(!blacklist) {
     blacklist = ['hollow knight', 'silksong', 'team cherry', 'hornet', 'nail', 'moss mother', 'carmelita', 'lace', 'sharpe', 'seth', 'trobbio', 'last judge', 'bell beast'];
 }
 
-function blockSpoilers(){
-    const elements = document.querySelectorAll(".ytd-rich-item-renderer:not(.ytd-rich-item-renderer .ytd-rich-item-renderer), yt-lockup-view-model, ytd-video-renderer, ytm-shorts-lockup-view-model-v2");
+const callback = (mutationList, observer) => {
+    for(const mutation of mutationList) {
+        if(mutation.type === "childList") {
+            mutation.addedNodes.forEach(node => {
+                if(node.matches?.(youtubeVideoClasses)) {
+                    if(blacklist.some(bl => node.textContent?.toLowerCase().includes(bl))) {
+                        let images = node.querySelectorAll('img, yt-image, .ytp-videowall-still-image, .ytCoreImageHost');
+                        if(images) images.forEach(img => {
+                            img.style.filter = 'blur(100px)';
+                        });
 
-    const matches = Array.from(elements).filter(el =>
-        blacklist.some(kw => el.innerText?.toLowerCase().includes(kw))
-    );
+                        let title = node.querySelectorAll('.yt-core-attributed-string, .ytp-videowall-still-info-title');
+                        if(title) title.forEach(txt => {
+                            txt.innerHTML = 'SPOILER';
+                        });
 
-    matches.forEach(el => {
-        let images = el.querySelectorAll('img, yt-image');
-        if(images) images.forEach(img => {
-            img.style.filter = 'blur(100px)';
-        });
-
-        let title = el.querySelector('.yt-core-attributed-string');
-        if(title) title.innerText = 'SPOILER';
-
-        let description = el.querySelectorAll('yt-formatted-string.ytd-video-renderer, yt-formatted-string.ytd-expandable-metadata-renderer');
-        if(description) description.forEach(txt => {
-            txt.innerHTML = 'SPOILER';
-        });
-
-        //el.style.border = '1px yellow solid';
-        //el.style.borderRadius = '12px';
-    });
-}
-
-const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.addedNodes.length) {
-            mutation.addedNodes.forEach((node) => {
-                if (node.nodeType === 1) {
-                    blockSpoilers();
+                        let description = node.querySelectorAll('yt-formatted-string.ytd-video-renderer, yt-formatted-string.ytd-expandable-metadata-renderer');
+                        if(description) description.forEach(txt => {
+                            txt.innerHTML = 'SPOILER';
+                        });
+                    }
                 }
             });
         }
-    });
-});
+    }
+};
 
-observer.observe(document.body, {childList: true, subtree: true});
+const observer = new MutationObserver(callback);
+observer.observe(content, {childList: true, subtree: true});
